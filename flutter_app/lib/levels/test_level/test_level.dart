bd84b768f0d201d1fea4088db50c0fe50cae0063
jdevacc20 <jdevacc20@gmail.com>
2023-02-25 10:34:38 +0100

    basic theme setup

--- flutter_app/lib/levels/test_level/test_level.dart
+++ flutter_app/lib/levels/test_level/test_level.dart
@@ -14,6 +14,7 @@ class TestLevel extends StatelessWidget {
             onPressed: () {
               value.increaseLevel();
             },
+            color: Theme.of(context).primaryColor,
             child: const Text("Fertig!"),
           ),
         ),
