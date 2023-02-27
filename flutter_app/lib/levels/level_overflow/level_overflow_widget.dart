de2a676f82b4f32c95b3a7402efc8246418087be
felix.wilde@mailbox.tu-dresden.de <thyrienjackson@gmail.com>
2023-02-27 19:40:05 +0100

    wrapped Overflow

--- flutter_app/lib/levels/level_overflow/level_overflow_widget.dart
+++ flutter_app/lib/levels/level_overflow/level_overflow_widget.dart
@@ -1,46 +1,56 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/models/level_model.dart';
+import 'package:flutter_app/levels/level_wrapper.dart';
 import 'package:flutter_app/levels/level_overflow/level_overflow_input_widget.dart';
-import 'package:provider/provider.dart';
 
-class LevelOverflowWidget extends StatelessWidget {
+class LevelOverflowWidget extends StatefulWidget {
+  const LevelOverflowWidget({super.key});
+
+  @override
+  State<LevelOverflowWidget> createState() => _LevelOverflowWidgetState();
+}
+
+class _LevelOverflowWidgetState extends State<LevelOverflowWidget> {
+  bool done = false;
   final _formKey = GlobalKey<FormState>();
-
-  LevelOverflowWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Consumer<LevelModel>(
-          builder: (context, value, child) => Center(
-                child: Form(
-                    key: _formKey,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-                        LevelOverflowInputWidget(
-                          labelLeft: "name:",
-                          labelRight: "gender:",
-                        ),
-                        LevelOverflowInputWidget(
-                          labelLeft: "age:",
-                          labelRight: "birth month:",
-                        ),
-                        LevelOverflowInputWidget(
-                          labelLeft: "username:",
-                          labelRight: "favourite color:",
-                          overflowInput: true,
-                        ),
-                        ElevatedButton(
-                            onPressed: () {
-                              if (_formKey.currentState!.validate()) {
-                                value.increaseLevel();
-                              }
-                            },
-                            child: const Text("Submit"))
-                      ],
-                    )),
-              )),
+    return LevelWrapper(
+      title: "Your Credentials",
+      description: "Please fill out the form",
+      done: done,
+      levelChild: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              LevelOverflowInputWidget(
+                labelLeft: "name:",
+                labelRight: "gender:",
+              ),
+              LevelOverflowInputWidget(
+                labelLeft: "age:",
+                labelRight: "birth month:",
+              ),
+              LevelOverflowInputWidget(
+                labelLeft: "username:",
+                labelRight: "favourite color:",
+                overflowInput: true,
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      setState(() {
+                        done = true;
+                      });
+                    }
+                  },
+                  child: const Text("Submit"))
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
